57e068a0e42862e2530d01bb37b792244027333e
kam <13710414456@163.com>
2018-02-04 19:08:54 +0800

    修改注释

--- web-launcher/src/main/java/org/exstension/web/WebService.java
+++ web-launcher/src/main/java/org/exstension/web/WebService.java
@@ -47,7 +47,7 @@ public class WebService {
             });
         } else {
             VertxUtils.setVertx(vertxOptions == null ? Vertx.vertx() : Vertx.vertx(vertxOptions));
-            logger.info("Web-Launch ->> launch app success.");
+            logger.info("Web-Launch ->> Vertx Component success.");
         }
     }
 
